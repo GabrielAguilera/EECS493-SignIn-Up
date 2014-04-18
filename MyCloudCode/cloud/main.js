6fb5d6cb6553650fd728a147bb7326abb02f7dd2
Gabriel Aguilera <gabrieag@umich.edu>
2014-04-18 15:48:27 -0400

    Cloud code updated and accepting is working.

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,6 +1,136 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("acceptFriend", function(request, response, acceptedFriend,
+													updatedFriends, updatedSent) {
+  
+  // The rest of the function operates on the assumption that request.user is *authorized*
+
+  Parse.Cloud.useMasterKey();
+
+  // Query for the user to be modified by username
+  // The username is passed to the Cloud Function in a 
+  // key named "username". You can search by email or
+  // user id instead depending on your use case.
+
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("username", request.params.acceptedFriend);
+
+  // Get the first user which matches the above constraints.
+  query.first({
+    success: function(anotherUser) {
+      // Successfully retrieved the user.
+      // Modify any parameters as you see fit.
+      // You can use request.params to pass specific
+      // keys and values you might want to change about
+      // this user.
+      //anotherUser.set("someKey", "someValue");
+      anotherUser.set("sentRequests", request.params.updatedSent);
+      anotherUser.set("friendUsernames", request.params.updatedFriends);
+      // Save the user.
+      anotherUser.save(null, {
+        success: function(anotherUser) {
+          // The user was saved successfully.
+          response.success("Successfully updated user.");
+        },
+        error: function(error) {
+          // The save failed.
+          // error is a Parse.Error with an error code and description.
+          response.error("Could not save changes to user.");
+        }
+      });
+    },
+    error: function(error) {
+      response.error("Could not find user.");
+    }
+  });
 });
+
+Parse.Cloud.define("rejectFriend", function(request, response,
+												rejectedFriend, updatedSent) {
+  
+  // The rest of the function operates on the assumption that request.user is *authorized*
+
+  Parse.Cloud.useMasterKey();
+
+  // Query for the user to be modified by username
+  // The username is passed to the Cloud Function in a 
+  // key named "username". You can search by email or
+  // user id instead depending on your use case.
+
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("username", request.params.rejectedFriend);
+
+  // Get the first user which matches the above constraints.
+  query.first({
+    success: function(anotherUser) {
+      // Successfully retrieved the user.
+      // Modify any parameters as you see fit.
+      // You can use request.params to pass specific
+      // keys and values you might want to change about
+      // this user.
+      //anotherUser.set("someKey", "someValue");
+      anotherUser.set("sentRequests", request.params.updatedSent);
+      // Save the user.
+      anotherUser.save(null, {
+        success: function(anotherUser) {
+          // The user was saved successfully.
+          response.success("Successfully updated user.");
+        },
+        error: function(error) {
+          // The save failed.
+          // error is a Parse.Error with an error code and description.
+          response.error("Could not save changes to user.");
+        }
+      });
+    },
+    error: function(error) {
+      response.error("Could not find user.");
+    }
+  });
+});
+
+Parse.Cloud.define("sendRequest", function(request, response,
+												desinationFriend, updatedPending) {
+  
+  // The rest of the function operates on the assumption that request.user is *authorized*
+
+  Parse.Cloud.useMasterKey();
+
+  // Query for the user to be modified by username
+  // The username is passed to the Cloud Function in a 
+  // key named "username". You can search by email or
+  // user id instead depending on your use case.
+
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("username", request.params.desinationFriend);
+
+  // Get the first user which matches the above constraints.
+  query.first({
+    success: function(anotherUser) {
+      // Successfully retrieved the user.
+      // Modify any parameters as you see fit.
+      // You can use request.params to pass specific
+      // keys and values you might want to change about
+      // this user.
+      //anotherUser.set("someKey", "someValue");
+      anotherUser.set("pendingRequests", request.params.updatedPending);
+      // Save the user.
+      anotherUser.save(null, {
+        success: function(anotherUser) {
+          // The user was saved successfully.
+          response.success("Successfully updated user.");
+        },
+        error: function(error) {
+          // The save failed.
+          // error is a Parse.Error with an error code and description.
+          response.error("Could not save changes to user.");
+        }
+      });
+    },
+    error: function(error) {
+      response.error("Could not find user.");
+    }
+  });
+});
+
